37bf943eac3d0eaadfc2b40a6c62cb6c3abd8651
SimonMoinetUnity <simonmoinet79@gmail.com>
2024-02-02 16:11:51 +0100

    Ajout de l'observable interval

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./observable.component.scss']
 })
 export class ObservableComponent {
+
+  interval$ = new Observable<number>;
+  chrono!:number;
+
+  ngOnInit(){
+    this.interval$ = interval(1000).pipe(
+      map(data => data*10),
+      filter(data => data < 50),
+      tap(data => console.log(data))
+    )
+
+    this.interval$.subscribe(data => this.chrono = data);
+
+    
+
+  }
 
 }
